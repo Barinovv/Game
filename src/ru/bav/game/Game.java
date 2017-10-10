8914fb37fd59ca4416007220ee3e4ebbf9f293ae
Barinovv <p.toxa@bk.ru>
2017-10-10 14:34:04 +0300

    Добавленны комментарии к классам Game и Ship.

--- src/ru/bav/game/Game.java
+++ src/ru/bav/game/Game.java
@@ -3,6 +3,12 @@ package ru.bav.game;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Класс для задания местоположения корабля и выстрела в него.
+ *
+ * @author Barinov Anton 1518
+ */
+
 public class Game {
     public static void main(String[] args) {
         ArrayList<String> localOfShip = new ArrayList<>();
@@ -16,6 +22,11 @@ public class Game {
         }
     }
 
+    /**
+     * Метод для задания местоположения корабля с помощью рандома
+     *
+     * @param locational - хранит месторасположение корабля
+     */
     public static void randomLocational(ArrayList<String> locational) {
         locational.add(String.valueOf((int) ((Math.random() * 4))));
         locational.add(String.valueOf(Integer.parseInt(locational.get(0)) + 1));
@@ -23,6 +34,11 @@ public class Game {
 
     }
 
+    /**
+     * Метод для ввода координаты выстрела в корабль.
+     *
+     * @return - возвращает введеную пользоватем координату выстрела.
+     */
     public static String scan() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите координату выстрела от 0 до 6: ");
--- src/ru/bav/game/Ship.java
+++ src/ru/bav/game/Ship.java
@@ -2,6 +2,11 @@ package ru.bav.game;
 
 import java.util.ArrayList;
 
+/**
+ * Класс для установления местоположения корабля и определения попадания в него.
+ *
+ * @author Barinov Anton 1518
+ */
 public class Ship {
     private ArrayList<String> location;
 
@@ -12,6 +17,14 @@ public class Ship {
     public ArrayList<String> getLocation() {
         return location;
     }
+
+    /**
+     * Метод для установления попадания/промаха/потопления (в) корабль.
+     *
+     * @param shot переменная которая хранит ввденную пользователем координату выстрела.
+     *
+     * @return результат выстрела.
+     */
 
     public String checkShip(String shot) {
         String result = "Мимо";
@@ -28,6 +41,7 @@ public class Ship {
         if (location.isEmpty()) {
             result = "Потопил";
         }
+
         return result;
 
     }
